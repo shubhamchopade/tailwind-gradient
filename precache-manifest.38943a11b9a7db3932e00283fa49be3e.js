7c1f8f0aabc4dd834be596c4dcfcde48e4ac85f3
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2020-10-24 04:27:41 +0530

    Updates

---
+++ precache-manifest.38943a11b9a7db3932e00283fa49be3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197b6fab6e84b2d2a897c007df6d7bb5",
+    "url": "/tailwind-gradient/index.html"
+  },
+  {
+    "revision": "91e96b97df70ad6efdab",
+    "url": "/tailwind-gradient/static/css/main.3f4da9c7.chunk.css"
+  },
+  {
+    "revision": "12d5923660e63657be59",
+    "url": "/tailwind-gradient/static/js/2.ad256c1e.chunk.js"
+  },
+  {
+    "revision": "5d091aea7ed00ab43a92de6446f31ff3",
+    "url": "/tailwind-gradient/static/js/2.ad256c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e96b97df70ad6efdab",
+    "url": "/tailwind-gradient/static/js/main.3659b704.chunk.js"
+  },
+  {
+    "revision": "4c019e7addb51c19dfc6",
+    "url": "/tailwind-gradient/static/js/runtime-main.adcadf26.js"
+  },
+  {
+    "revision": "a46d4d8c04ca487abf8eb983649b4b15",
+    "url": "/tailwind-gradient/static/media/head-img.a46d4d8c.svg"
+  }
+]);
