55448c46ab2253273e8f50746eb44c5034447c41
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2021-02-03 19:07:13 +0530

    firebase analytics

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -14,6 +14,7 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const projectStorage = firebase.storage();
 const projectFireStore = firebase.firestore();
