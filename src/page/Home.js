240f2185102cf6f4571263dcc99570972adcbb93
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2021-04-17 18:27:35 +0530

    removed transition

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
 import RandomGradient from "../components/RandomGradient";
 import BrowseGradient from "../components/BrowseGradient";
 import { StyledLink } from "../components/theme";
@@ -13,28 +11,8 @@ const Home = () => {
   const handleRandom = () => setToggle(true);
   const handleBrowse = () => setToggle(false);
 
-  const pageVariants = {
-    initial: {
-      opacity: 0,
-    },
-    in: {
-      opacity: 1,
-      transition: { ease: "easeIn", duration: 2 },
-    },
-    out: {
-      opacity: 0,
-      x: "-100vw",
-      transition: { ease: "easeIn", duration: 1.5 },
-    },
-  };
-
   return (
-    <motion.div
-      variants={pageVariants}
-      initial="initial"
-      animate="in"
-      exit="out"
-    >
+    <motion.div>
       <div className="flex justify-around max-w-6xl mx-auto ">
         <StyledLink onClick={handleRandom} toggle={toggle}>
           Generate Random Gradient
